eccc1e6a1de8195cd96d2cde7eb3cd6df25762ce
Pedro Escapalete <fabricio.pimentel@edu.senai.br>
2026-05-21 14:42:13 -0300

    Exemplos de array....

---
+++ Exemplo array/exemplos.js
@@ -0,0 +1,45 @@
+//Fabricio
+
+//Git hub - Exemplos de array
+
+let sucos = ["laranja", "uva", "abacaxi", "manga", "maracuja"]
+console.log(sucos[0])
+
+
+//array de numeros
+let numreos = [1, 2, 3, 4, 5]
+console.log(numreos[2])
+
+//elemento especifico num array
+let animais = ["cachorro", "gato", "passaro", "peixe"]
+console.log (ANIMAIS[1])
+
+//ALTERANDO UM  elemento do array
+let times = ["Flamengo", "Palmeiras", "São Paulo", "Corinthians"]
+times[2] = "Vasco"
+console.log(times)
+
+//Tamanho do array
+let games = ["FIFA", "COD", "Minecraft", "Fortnite"]
+console.log(games.lenght)
+
+//Adicionando elemento final do array
+let cores = ["Vermelho", "Azul", "Verde"]
+cores.push("Amarelo")
+console.log(cores)
+
+//Removendo o ultimo elemento do array
+let carros = ["Fusca", "Gol", "Civic", "Corolla"]
+carros.pop()
+console.log(carros)
+
+//Removendo primeiro elemento do array
+let cidades = ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba"]
+cidades.shift()
+console.log(cidades)
+
+//Adicionando um elemento no ínicio do array
+let paises = ["Brasil", "Argentina", "Chile"]
+paises.unshift("Uruguai")
+console.log(paises)
+
